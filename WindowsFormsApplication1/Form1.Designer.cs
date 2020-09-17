aa1df34b8cdb548566a68a935542371ff87407f7
Oleg Davydow <zyxorus@mail.ru>
2020-09-17 15:28:22 +0500

    Добавил GridView. Теперь данные из таблицы Physical_Person выгружаются

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -31,13 +31,14 @@
             this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.button2 = new System.Windows.Forms.Button();
             this.wMF300DataSet = new WindowsFormsApplication1.WMF300DataSet();
             this.identeficationBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.identeficationTableAdapter = new WindowsFormsApplication1.WMF300DataSetTableAdapters.IdenteficationTableAdapter();
             this.tableAdapterManager = new WindowsFormsApplication1.WMF300DataSetTableAdapters.TableAdapterManager();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.wMF300DataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.identeficationBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -58,15 +59,6 @@
             this.comboBox1.Size = new System.Drawing.Size(249, 21);
             this.comboBox1.TabIndex = 1;
             this.comboBox1.Text = "Физическое_лицо";
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(754, 50);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
             // 
             // wMF300DataSet
             // 
@@ -97,13 +89,21 @@
             this.tableAdapterManager.Technic_NameTableAdapter = null;
             this.tableAdapterManager.UpdateOrder = WindowsFormsApplication1.WMF300DataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 32);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(743, 295);
+            this.dataGridView1.TabIndex = 2;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(114)))), ((int)(((byte)(157)))));
             this.ClientSize = new System.Drawing.Size(863, 353);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -111,6 +111,7 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.wMF300DataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.identeficationBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -119,11 +120,11 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Button button2;
         private WMF300DataSet wMF300DataSet;
         private System.Windows.Forms.BindingSource identeficationBindingSource;
         private WMF300DataSetTableAdapters.IdenteficationTableAdapter identeficationTableAdapter;
         private WMF300DataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -12,6 +12,14 @@ namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
+        private SqlConnection SqlConnection = null;
+
+        private SqlCommandBuilder sqlBuilder = null;
+
+        private SqlDataAdapter sqlDataAdapter = null;
+
+        private DataTable table = null;
+
         public Form1()
         {
             InitializeComponent();
@@ -25,6 +33,12 @@ namespace WindowsFormsApplication1
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            SqlConnection = new SqlConnection(@"Data Source=.\SQLEXPRESS;AttachDbFilename=C:\Users\ZyxoR\source\repos\WindowsFormsApplication1\WindowsFormsApplication1\WMF300.mdf;Integrated Security=True;Connect Timeout=30;User Instance=True");
+            SqlConnection.Open();
+            sqlDataAdapter = new SqlDataAdapter("SELECT * FROM Physical_Person", SqlConnection);
+            table = new DataTable();
+            sqlDataAdapter.Fill(table);
+            dataGridView1.DataSource = table;
             // TODO: данная строка кода позволяет загрузить данные в таблицу "wMF300DataSet.Identefication". При необходимости она может быть перемещена или удалена.
             this.identeficationTableAdapter.Fill(this.wMF300DataSet.Identefication);
             comboBox1.Items.Add("Физическое_Лицо");
@@ -46,5 +60,18 @@ namespace WindowsFormsApplication1
             this.tableAdapterManager.UpdateAll(this.wMF300DataSet);
 
         }
-    }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                MessageBox.Show("vscode.ru");
+            }
+            if (comboBox1.SelectedIndex == 1)
+            {
+                MessageBox.Show("Уроки программирования");
+            }
+        }
+
+        }
 }
